91096c4c0167cb44af79444f01ff0b7a914626f0
shayanchakraborty18 <shayanchakraborty@Shayans-MacBook-Air.local>
2025-06-24 04:00:58 +0530

    Task done 1) DB setup 2) Global Error handler Setup 3) Logger Middleware 4) Swagger Setup

---
+++ config/mongodbConfig.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const DB_URL = process.env.DB_URL || `mongodb://0.0.0.0:27017`;
+
+const FULL_URL = DB_URL + '/Postway';
+
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(FULL_URL);
+		console.log("Database Connection Successful");
+	} catch (err) {
+		throw new Error(`Error in Database Connection ${err}`);
+	}
+}
+export default connectToMongoDB;
---
+++ middlewares/logger.middleware.js
@@ -0,0 +1,19 @@
+import winston from "winston";
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	defaultMeta: { service: "request-logging" },
+	transports: [
+		new winston.transports.File({ filename: "logs.txt" })
+	]
+});
+
+const loggerMiddleware = async (req, res, next) => {
+	if(!req.url.includes('signin')) {
+		const logData = `${req.url} - ${JSON.stringify(req.body)}`;
+		logger.info(logData);
+	}
+	next();
+};
+
+export default loggerMiddleware;
